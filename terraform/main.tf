1c95952251aa34186635443704cf1d7da65035c6
Anusri P <anusrip14@gmail.com>
2026-02-06 11:25:39 +0000

    Update key_name in main.tf to 'key'

--- terraform/main.tf
+++ terraform/main.tf
@@ -177,7 +177,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
-  key_name = "lab_test_pem"   # ← use your existing AWS key pair name
+  key_name = "key"   # ← use your existing AWS key pair name
 
   tags = {
     Name = "streamline-web-${count.index + 1}"
